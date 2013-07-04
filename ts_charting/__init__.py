7f876881267da77efeb8c3f5bb585502e33e76fc
Dale Jung <dale@dalejung.com>
2013-07-04 01:07:14 -0400

    add imagefile funcs to namespace

--- ts_charting/__init__.py
+++ ts_charting/__init__.py
@@ -7,3 +7,4 @@ import ts_charting.span
 from ts_charting.styler import styler, marker_styler, level_styler
 from ts_charting.ipython import figsize, IN_NOTEBOOK
 from ts_charting.plot_3d import plot_wireframe
+from ts_charting.imagefile import plot_pdf, save_images
