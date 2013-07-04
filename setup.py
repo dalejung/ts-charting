00e59418d56b49e19b8565b36f31181a595c5e4d
Dale Jung <dale@dalejung.com>
2013-07-03 23:09:08 -0400

    addes setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup                                                      
+                                                                                      
+DISTNAME='ts_charting'                                                                 
+FULLVERSION='0.1'                                                                     
+                                                                                      
+setup(name=DISTNAME,                                                                  
+      version=FULLVERSION,                                                            
+      packages=['ts_charting',
+               ]                                                         
+      )   
