538d0b4799dfd70702c748df62cd491481e16d7f
Dale Jung <dale@dalejung.com>
2013-08-01 23:10:27 -0400

    added json support

---
+++ ts_charting/json.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from pandas import json
+from IPython.display import JSON
+
+def dataframe_json(df):
+    data = {}
+    for k, v in df.iteritems():
+        data[k] = v.values
+    data['index'] = df.index
+    data['_pandas_type'] = 'dataframe';
+    data['__repr__'] = repr(df)
+    return json.dumps(data)
+
+def series_json(series):
+    data = {}
+    data['data'] = series.values
+    data['index'] = series.index
+    data['name'] = series.name
+    data['_pandas_type'] = 'series'
+    data['__repr__'] = repr(series)
+    return json.dumps(data)
+
+def to_json(obj):
+    if isinstance(obj, pd.DataFrame):
+        return dataframe_json(obj)
+
+    if isinstance(obj, pd.Series):
+        return series_json(obj)
+
+    if isinstance(obj, list):
+        jlist = []
+        for v in obj:
+            jlist.append(to_json(v))
+        return json_list(jlist)
+
+    if isinstance(obj, dict):
+        jdict = {}
+        for k, v in obj.iteritems():
+            jdict[k] = to_json(v)
+        return json_dict(jdict)
+
+    if hasattr(obj, 'to_json'):
+        return obj.to_json()
+
+    return json.dumps(obj)
+
+def json_dict(dct):
+    items = []
+    for k, v in dct.iteritems():
+        items.append("\"{k}\":{v}".format(k=k, v=v))
+    return "{" + ','.join(items) + "}" 
+
+def json_list(lst):
+    return "[" + ','.join(lst) + "]" 
+
+def to_json_display(obj):
+    return JSON(to_json(obj));
--- ts_charting/lab/lab.py
+++ ts_charting/lab/lab.py
@@ -1,6 +1,9 @@
 from collections import OrderedDict
+from ts_charting import json
 
-from ts_charting import Figure, Grapher, gcf, scf
+from ts_charting import Figure, scf
+from ts_charting.ohlc import normalize_ohlc
+from ts_charting.util import process_signal
 
 class Lab(object):
     def __init__(self):
@@ -14,36 +17,59 @@ class Lab(object):
         scf(station)
         return station
 
+    def to_json(self):
+        dct = {}
+        dct['stations'] = self.stations
+        return json.to_json(dct)
+
 class Station(object):
     def __init__(self, lab, name):
         self.lab = lab
         self.name = name
         self.figure = Figure(1, warn=False)
-        self.layers = OrderedDict()
+        self.layers = []
 
     def plot_markers(self, name, series, yvalues=None, xindex=None, **kwargs):
         geom = {'type': 'marker'}
-        geom['yvalues'] = yvalues
-        geom['xindex'] = xindex
+        # dont support xindex for now
+        #geom['xindex'] = xindex
         geom.update(kwargs)
 
-        self.layer(name, series, geom)
-        self.figure.plot_markers(name, series, yvalues=yvalues, xindex=xindex, **kwargs)
-
-    def __getattr__(self, name):
-        if hasattr(self.figure, name):
-            return getattr(self.figure, name)
-        raise AttributeError()
+        if yvalues is not None:
+            series = process_signal(series, yvalues)
+        self.add_layer(name, series, geom)
+        self.figure.plot_markers(name, series, **kwargs)
 
     def ohlc(self, df, width=0.3):
-        self.layer('candlestick', df, {'type': 'candlestick', 'width': .03})
+        # ohlc_df = normalize_ohlc(df)
+        self.add_layer('candlestick', df, {'type': 'candlestick', 'width': .03})
         self.figure.ohlc(df, width=width)
 
-    def layer(self, name, data, geoms):
+    def add_layer(self, name, data, geoms):
         if not isinstance(geoms, list):
             geoms = [geoms]
 
-        if name in self.layers:
-            raise Exception("Already have named layer, don't support updates yet")
+        self.layers.append({'name': name, 'data': data, 'geoms':geoms})
 
-        self.layers[name] = {'data': data, 'geoms':geoms}
+    def to_json(self):
+        dct = self.__dict__.copy()
+        del dct['figure']
+        del dct['lab']
+        index = self.consolidate_index()
+        dct['index'] = index
+        return json.to_json(dct)
+
+    def consolidate_index(self):
+        """
+        Take the index of every layer's data and create a single index. Currently 
+        acts like the regular Grapher and uses the first data's index as the master
+        index. All other indexes get reindex to that one
+        """
+        index = None
+        for layer in self.layers:
+            if index is None:
+                index = layer['data'].index
+                continue
+            # reindex the layer data and any yvalues passed into its geoms
+            layer['data'] = layer['data'].reindex(index) 
+        return index
